6e167f1a5902f75503edf1c20f02d31774b78d12
trangnguyen <nttrang30792@gmail.com>
2020-10-24 17:31:53 +0200

    iteration 3,4 ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import contacts from './contacts.json';
+// import contacts from './contacts.json';
 import List from './component/cardList'
 
 function App() {
