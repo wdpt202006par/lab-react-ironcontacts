eabf45277ee8f828a27ba556f73c12953b83097f
Clementine <clementinenocton@hotmail.fr>
2020-10-24 16:14:56 +0200

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    contacts: contacts.slice(0, 5)
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>IronContacts</h1>
+        <table>
+          <thead>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </thead>
+          <tbody>
+            {this.state.contacts.map(contact => (
+              <tr>
+                <td><img src={contact.pictureUrl} alt="Contact"/></td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App;
