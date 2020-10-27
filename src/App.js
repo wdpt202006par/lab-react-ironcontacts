5129f13e6d4e46eb5d0630469f484256dc65ed28
psophielam <psophielam92@gmail.com>
2020-10-27 19:17:01 +0100

    iteration 2 ongoing

--- src/App.js
+++ src/App.js
@@ -4,45 +4,35 @@ import './App.css';
 import ContactsJson from './contacts.json';
 import Contacts from './components/contact.js';
 
+class App extends React.Component {
 
 
-function App() {
-  let contactArray = [
-    {
-      "name": "Idris Elba",
-      "pictureUrl": "https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg",
-      "popularity": 11.622713,
-      "id": "11731993-0604-4bee-80d5-67ad845d0a38"
-    },
-    {
-      "name": "Johnny Depp",
-      "pictureUrl": "https://image.tmdb.org/t/p/w500/kbWValANhZI8rbWZXximXuMN4UN.jpg",
-      "popularity": 15.656534,
-      "id": "7dad00f7-3949-477d-a7e2-1467fd2cfc06"
-    },
-    {
-      "name": "Monica Bellucci",
-      "pictureUrl": "https://image.tmdb.org/t/p/w500/qlT4904d8oi2NIs28RrgnIZDFZB.jpg",
-      "popularity": 16.096436,
-      "id": "0ad5e441-3084-47a1-9e9b-b917539bba71"
-    },
-    {
-      "name": "Gal Gadot",
-      "pictureUrl": "https://image.tmdb.org/t/p/w500/34kHAyBaBhq2kUrmhM15paEBuuI.jpg",
-      "popularity": 10.049256,
-      "id": "b497e3c4-50bb-4ae2-912f-eb36802c5bc2"
-    },
-    {
-      "name": "Ian McKellen",
-      "pictureUrl": "https://image.tmdb.org/t/p/w500/coWjgMEYJjk2OrNddlXCBm8EIr3.jpg",
-      "popularity": 10.070132,
-      "id": "0067ae32-97b6-4431-898e-eb1c10150abb"
-    }
-  ]
+constructor(props) {
   
+  super(props);
+  
+  this.state = {contacts:ContactsJson.slice(0,5)}
+    // name: props.name,
+    // pictureUrl: props.pictureUrl,
+    // popularity : props.popularity,
+    // id: props.id
+}
+
+addButton(){
+  let randomContact = ContactsJson[Math.floor(Math.random()* ContactsJson.length)]
+  this.setState({
+contacts : [...this.state.contacts,randomContact]
+
+  })
+}
+
+render(){
   return (
     <div className="App">
+    
     <h1>Ironcontacts</h1>
+    <button onClick ={this.addButton}>Add a random contact</button>
+
 <table>
         <thead>
             <tr>
@@ -52,7 +42,7 @@ function App() {
             </tr>
         </thead>
         <tbody>
-          {contactArray.map(contact => (
+          {this.state.contacts.map(contact => (
             <Contacts {...contact}/>
           ))}
         </tbody>
@@ -61,6 +51,7 @@ function App() {
 
     </div>
   );
+}
 }
 
 export default App;
